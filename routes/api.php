95305d7a95e93d742fca9c45403b028c448a6342
Vaghik <vaghiksoghomonyan@gmail.com>
2020-08-01 09:15:29 +0400

    init

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => ['jwt.auth','api-header']], function () {
+    Route::get('users/list', function(){
+        $users = App\User::all();
+        
+        $response = ['success'=>true, 'data'=>$users];
+        return response()->json($response, 201);
+    });
+});
+
+Route::group(['middleware' => 'api-header'], function () {
+    Route::post('user/login', 'UserController@login');
+    Route::post('user/register', 'UserController@register');
+});
